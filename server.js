1b91030523c0141cf6799e96a5f8998cadff7195
Rômulo Andrade <56308434+andradeoromulo@users.noreply.github.com>
2020-11-24 14:12:46 -0300

    Add basic configuration and db connection

--- server.js
+++ server.js
@@ -1,24 +1,47 @@
-require('dotenv').config();
-const express = require('express');
+// Imports
+const mongoose = require('mongoose');
 const cors = require('cors');
+const express = require('express');
 const app = express();
+
+// Environment variables
+require('dotenv').config();
+
+// Database connection
+const uri = process.env.ATLAS_URI;
+const connectionParams = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+};
+
+mongoose.connect(uri, connectionParams)
+  .then(() =>
+    console.log("Database successfully connected")
+  )
+  .catch((err) => 
+    console.log(`Error connecting to the database:\n${err}`)  
+  );
+
+const connection = mongoose.connection;
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+app.use(cors({optionsSuccessStatus: 200}));
 
-app.use(cors());
+// Static files
+app.use(express.static(`${__dirname}/public`));
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
+// Routing
 app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(`${__dirname}views/index.html`);
 });
 
-// Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// Server listening
 app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
